e25f321248f84c6980fa6e0690563c0427876285
Kaiper09 <gunnarkaipper@gmail.com>
2025-04-30 21:34:46 -0300

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,11 @@
+const {contextBridge, ipcRenderer} = require('electron')
+
+function selectFile(){
+    return ipcRenderer.invoke('select-file');
+}
+
+
+
+contextBridge.exposeInMainWorld('api',{
+    selectFile: selectFile
+})
